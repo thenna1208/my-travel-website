8fbe605bd4b508089215cbe41559fde83a565809
thenna1208 <thennavan.m@gmail.com>
2023-03-25 18:09:01 +0530

    sign up changes

---
+++ src/components/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+   {
+    title: 'Home',
+    cName: 'nav-links',
+    url: '/'
+   },
+   {
+    title: 'About',
+    cName: 'nav-links',
+    url: '/about'
+   },
+   {
+    title: 'Services',
+    cName: 'nav-links',
+    url: '/service'
+   }, 
+   {
+    title: 'Contact',
+    cName: 'nav-links',
+    url: '/contact'
+   },
+   {
+      title: 'Sign-up',
+      cName: 'nav-links-signup',
+      url: '/signup'
+     }
+
+  ];
