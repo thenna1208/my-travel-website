63e36d60299e95a3de9bdae9d97bf825ea42a0f8
thenna1208 <thennavan.m@gmail.com>
2023-03-25 18:21:37 +0530

    test

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -8,7 +8,7 @@ function AboutUs() {
       Our History
       </h1>
       <p>
-      Trippy is owned and managed by Trippy .In Pvt. Ltd., a leading brand in web designing services and e-commerce solutions. Trippy .In Pvt. Ltd. is counted for its expertise in web solutions and its top ranking business portals. Our invincible expertise and rich experience has raised our spirit to reach ahead from our client's expectation. Commendable success rate of other portals managed by Trippy is a live paradigm of our work excellence.
+      Trippy is owned and managed by Trippy.In Pvt. Ltd., a leading brand in web designing services and e-commerce solutions. Trippy .In Pvt. Ltd. is counted for its expertise in web solutions and its top ranking business portals. Our invincible expertise and rich experience has raised our spirit to reach ahead from our client's expectation. Commendable success rate of other portals managed by Trippy is a live paradigm of our work excellence.
       </p>
 
       <h1>
